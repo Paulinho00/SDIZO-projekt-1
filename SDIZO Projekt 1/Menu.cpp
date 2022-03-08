b8fecb6e650e6fe65a89c8608685f501f0993678
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2022-03-08 11:54:06 +0100

    Usuniecie DynamicArray, poprawienie kodu glownego menu

--- SDIZO Projekt 1/Menu.cpp
+++ SDIZO Projekt 1/Menu.cpp
@@ -1,43 +1,38 @@
 #include <iostream>
-#include "DynamicArray.h"
 using namespace std;
 
-//Funkcja wyswietlajaca menu
-void showDataStructuresMenu() {
-	cout << "Wybierz strukture:\n";
-	cout << "1. Tablica dynamiczna\n";
-	cout << "2. Lista dwukierunkowa\n";
-	cout << "3. Kopiec binarny\n";
-	cout << "4. Drzewo poszukiwan binarny (BST)\n";
-	cout << "5. Drzewo czerwono-czarne\n";
-	cout << "6. Zakoncz program\n";
-	cout << "Podaj odpowiednia liczbe: \n";
+//Funkcja wyswietlajaca menu i zarzadzajaca wyborem uzytkownika
+void dataStructuresMenu() {
+	while (1) {
+		//Wyswietlenie opcji w menu
+		cout << "\nWybierz strukture:\n";
+		cout << "1. Tablica dynamiczna\n";
+		cout << "2. Lista dwukierunkowa\n";
+		cout << "3. Kopiec binarny\n";
+		cout << "4. Drzewo poszukiwan binarny (BST)\n";
+		cout << "5. Drzewo czerwono-czarne\n";
+		cout << "0. Zakoncz program\n";
+		cout << "Podaj odpowiednia liczbe: \n";
+		
+		//Odczyt wyboru uzytkownika
+		int userInput;
+		cin >> userInput;
+
+		//Uruchomienie odpowiedniej funkcji w zaleznosci od wyboru
+		switch (userInput) {
+		case 0: exit(0);
+		default: cout << "Nie ma takiej oppcji\n"; break;
+		};
+
+		//Wyczyszczenie buforu
+		cin.clear();
+		cin.ignore(100, '\n');
+	}
 }
 
-//Funkcja odczytujaca wybor uzytkownika i uruchamiajaca odpowiednie funkcje
-void readUserInput() {
-	int* userInput = new int;
-	cin >> * userInput;
-
-	switch (*userInput) {
-	case 1: {
-		DynamicArray dynamicArray;
-		dynamicArray.handleMenu();
-	} break;
-	case 6: exit(0);
-	default: cout << "Nie ma takiej oppcji\n"; break;
-	};
-
-	delete userInput;
-	cin.clear();
-	cin.ignore(100, '\n');
-}
 
 int main() {
-	while (1) {
-		showDataStructuresMenu();
-		readUserInput();
-	}
+	dataStructuresMenu();
 }
 
 
