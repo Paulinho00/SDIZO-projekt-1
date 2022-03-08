90ab3750b5c9d09e3b3e4b5dfc5e2402d2cbbc22
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2022-03-08 12:11:02 +0100

    Dodanie menu tablicy dynamicznej

--- SDIZO Projekt 1/Menu.cpp
+++ SDIZO Projekt 1/Menu.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 using namespace std;
 
+//Wyswietlanie i zarzadzanie menu tablicy dynamicznej
+void dynamicArrayMenu() {
+	while (1) {
+		//Wyswietlenie opcji w menu
+		cout << "\nWybierz opcje:\n";
+		cout << "1. Odczytaj dane z pliku\n";
+		cout << "2. Dodaj element na koncu tablicy\n";
+		cout << "3. Dodaj element na wybranej pozycji\n";
+		cout << "5. Usun element na koncu tablicy\n";
+		cout << "6. Usun element na wybranej pozycji\n";
+		cout << "7. Wyswietl wszystkie wartosci\n";
+		cout << "8. Znajdz wartosc w tablicy\n";
+		cout << "0. Cofnif do menu glownego\n";
+		cout << "Podaj odpowiednia liczbe\n";
+
+		//Odczyt wyboru uzytkownika
+		int userInput;
+		cin >> userInput;
+
+		//Uruchomienie odpowiedniej funkcji w zaleznosci od wyboru
+		switch (userInput) {
+		case 0: return;
+		default: cout << "Nie ma takiej oppcji\n"; break;
+		};
+
+		//Wyczyszczenie buforu
+		cin.clear();
+		cin.ignore(100, '\n');
+	}
+}
+
 //Funkcja wyswietlajaca menu i zarzadzajaca wyborem uzytkownika
 void dataStructuresMenu() {
 	while (1) {
@@ -20,6 +51,7 @@ void dataStructuresMenu() {
 
 		//Uruchomienie odpowiedniej funkcji w zaleznosci od wyboru
 		switch (userInput) {
+		case 1: dynamicArrayMenu(); break;
 		case 0: exit(0);
 		default: cout << "Nie ma takiej oppcji\n"; break;
 		};
@@ -29,7 +61,6 @@ void dataStructuresMenu() {
 		cin.ignore(100, '\n');
 	}
 }
-
 
 int main() {
 	dataStructuresMenu();
